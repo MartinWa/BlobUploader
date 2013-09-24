c9d2c12a4c6c0de42a5f5b43bedc5a6a9d24c084
Martin Wåger <martin.wager@dynabyte.se>
2013-09-24 10:15:13 +0200

    UploadController

---
+++ AdminAppTest/Controllers/UploadController.cs
@@ -0,0 +1,17 @@
+﻿using System.Web.Mvc;
+
+namespace AdminAppTest.Controllers
+{
+    [Authorize(Roles = "Uploader")]
+    public class UploadController : Controller
+    {
+        //
+        // GET: /Upload/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
